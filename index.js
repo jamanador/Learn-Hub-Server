4768f752f10cb41367c95ad1362c4a0bf0990168
Ador Zamaan <adorzamaan@gmail.com>
2022-10-26 18:45:42 +0600

    C:/Program Files/Git/courses

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.port || 5000;
-// const courses = require("./data/courses.json");
+const courses = require("./data/courses.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(courses);
 });
-// app.get("/courses", (req, res) => {
-//   res.send(courses);
-// });
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
 // app.get("/courses/:id", (req, res) => {
 //   const id = req.params.id;
